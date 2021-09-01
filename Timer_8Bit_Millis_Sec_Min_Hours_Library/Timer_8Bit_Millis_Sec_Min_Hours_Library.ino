84341234b67eb558062c7288aec08ba082da35b2
emadjabbour <dadojab@gmail.com>
2021-09-01 14:44:00 +0300

    Add files via upload

---
+++ Timer_8Bit_Millis_Sec_Min_Hours_Library/Timer_8Bit_Millis_Sec_Min_Hours_Library.ino
@@ -0,0 +1,20 @@
+#include "TIMER.h"
+
+
+void setup() {
+  Serial.begin(115200);
+  TimerBegin();
+}
+void loop() {
+  Serial.println(mills);
+  Serial.println(sec);
+  Serial.println(mins);
+  Serial.println(hours);
+  delay(100);
+
+
+}
+
+
+
+
