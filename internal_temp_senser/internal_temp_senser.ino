4c3a8d57d2949bf8f2bba0758ad93d21eaefdd49
emadjabbour <dadojab@gmail.com>
2021-09-04 15:38:01 +0300

    Add files via upload

---
+++ internal_temp_senser/internal_temp_senser.ino
@@ -0,0 +1,16 @@
+#include "temp.h"
+
+void setup()
+{
+  Serial.begin(9600);
+
+  Serial.println(F("Internal Temperature Sensor"));
+}
+
+void loop()
+{
+  Serial.println(GetTemp(),1);
+  delay(1000);
+}
+
+
---
+++ internal_temp_senser/temp.h
@@ -0,0 +1,21 @@
+double GetTemp(void)
+{
+  unsigned int wADC;
+  double t;
+
+
+  ADMUX = (1<<(REFS1) | 1<<(REFS0) |1<<(MUX3));
+  ADCSRA |= 1<<(ADEN); 
+
+  delay(20);           
+
+  ADCSRA |= 1<<(ADSC);  
+
+  while (bit_is_set(ADCSRA,ADSC));
+
+  wADC = ADCW;
+
+  t = (wADC - 317 ) / 1.22;
+
+  return (t);
+}
